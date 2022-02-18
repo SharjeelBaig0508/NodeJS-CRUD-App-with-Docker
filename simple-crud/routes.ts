b3335491abd3818e1d5e45034d16d6ade2a6762a
SharjeelBaig <sharjeel@rippleuae.com>
2022-02-18 16:15:54 +0500

    added not found route functionality

--- simple-crud/routes.ts
+++ simple-crud/routes.ts
@@ -4,10 +4,11 @@ import validate from './middleware/validateResource';
 import { createUserSchema } from './schema/user.schema';
 
 const routes = (app: Express) => {
-    app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200) );
-
+    app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200));
 
     app.post('/api/users', validate(createUserSchema), createUserHandler);
+
+    app.all("*", (req: Request, res: Response) => res.sendStatus(404));
 }
 
 export default routes;
